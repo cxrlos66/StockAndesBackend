844279f8e8a5f1c4064123c26ffebda7c0b31de4
cxrlos66 <cxrlosat.17@gmail.com>
2026-09-25 11:53:09 -0500

    Delete sql directory

--- sql/crear_usuario.sql
+++
@@ -1,5 +0,0 @@
--- Ejecutar con un usuario administrador de Oracle si todavía no existe STOCKANDES.
--- Si ya tienes un usuario propio, puedes omitir este archivo y cambiar application-dev.yaml.
-
-CREATE USER STOCKANDES IDENTIFIED BY 1234567;
-GRANT CREATE SESSION, CREATE TABLE, CREATE SEQUENCE, CREATE TRIGGER, UNLIMITED TABLESPACE TO STOCKANDES;
